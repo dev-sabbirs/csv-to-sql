c01801255266a6914d80db515a991ab031a62a10
devlopersabbir <devlopersabbir@gmail.com>
2024-01-01 12:01:05 +0600

    [style 🔧] converted schema

--- src/schema/schemas.ts
+++ src/schema/schemas.ts
@@ -1,8 +1,17 @@
-import { pgTable, serial, text, varchar, uuid } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  serial,
+  text,
+  varchar,
+  uuid,
+  numeric,
+} from "drizzle-orm/pg-core";
 import {} from "drizzle-orm/pg-core";
 
 export const csvToSql = pgTable("csvToSql", {
   id: serial("id"),
   uuid: uuid("uuid").primaryKey().defaultRandom().unique(),
-  sqlFile: text("sqlFile").notNull(),
+  csvFile: varchar("sqlFile").notNull(),
+  sqlFileName: varchar("sqlFileName"),
+  converted: numeric("converted"),
 });
