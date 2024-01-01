6f905dc89e13152ccb03ba5a1c9af1777eacc0e0
devlopersabbir <devlopersabbir@gmail.com>
2024-01-01 12:27:10 +0600

    [feat ⅄] CSV to SQL schema

--- src/schema/schemas.ts
+++ src/schema/schemas.ts
@@ -1,17 +1,31 @@
+import { bigint } from "drizzle-orm/pg-core";
 import {
   pgTable,
   serial,
-  text,
+  timestamp,
   varchar,
   uuid,
-  numeric,
+  integer,
 } from "drizzle-orm/pg-core";
 import {} from "drizzle-orm/pg-core";
 
-export const csvToSql = pgTable("csvToSql", {
-  id: serial("id"),
-  uuid: uuid("uuid").primaryKey().defaultRandom().unique(),
-  csvFile: varchar("sqlFile").notNull(),
-  sqlFileName: varchar("sqlFileName"),
-  converted: numeric("converted"),
-});
+export const csvToSql = pgTable(
+  "csvToSql",
+  {
+    id: serial("id"),
+    uuid: uuid("uuid").primaryKey().defaultRandom().unique(),
+    inputFile: varchar("inputFile").notNull(),
+    outputFile: varchar("outputFile"),
+    converted: bigint("converted", {
+      mode: "number",
+    }).default(0),
+
+    createdAt: timestamp("createdAt", { withTimezone: true })
+      .defaultNow()
+      .notNull(),
+    updatedAt: timestamp("updatedAt", { withTimezone: true })
+      .defaultNow()
+      .notNull(),
+  },
+  (t) => ({})
+);
